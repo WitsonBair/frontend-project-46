3fbf4c694c138b6555b39b354ad46ca9557eb7b4
witsonbair <tsyden2.bair@gmail.com>
2022-12-31 18:51:55 +0800

    step 8.1

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -1,39 +1,52 @@
-const makeSpace = (depth) => ' '.repeat(depth * 4 - 2);
+import _ from 'lodash';
 
-const stringify = (obj, depth) => {
-  const space = makeSpace(depth + 1);
-  const preSpace = `${space}`;
-  
-  if (obj instanceof Object) {
-    const makeString = Object.entries(obj).map(([key, value]) => `${preSpace}${key}: ${stringify(value, depth + 1)}`);
-    return `{\n${makeString.join('\n')}\n${space}}`;
-  }
-  return obj;
-};
+const stylish = (tree) => {
+  const space = ' ';
+  const spaceSize = 4;
 
-const stylish = (tree, depth = 1) => {
-  const space = makeSpace(depth);
-  const preSpace = `${space}`;
+  const iter = (node, depth) => {
+    const indent = depth * spaceSize;
+    const closeIndent = depth * spaceSize - 2;
+    const currentIndent = space.repeat(indent);
+    const currentCloseIndent = space.repeat(closeIndent);
+    const bracketIndent = space.repeat(indent - spaceSize);
+    const iterIfObject = (value) => {
+      if (_.isObject(value)) {
+        return iter([value], depth + 1);
+      }
+      return value;
+    };
 
-  const result = tree.map((node) => {
-    switch (node.type) {
-      case 'plus':
-        return `${preSpace}+ ${node.name}: ${stringify(node.value, depth)}`;
-      case 'minus':
-        return `${preSpace}- ${node.name}: ${stringify(node.value, depth)}`;
-      case 'object':
-        return `${preSpace}  ${node.name}: {${stylish(node.children, depth + 1)}${makeSpace(depth + 1)}}`;
-      case 'same':
-        return `${preSpace}  ${node.name}: ${stringify(node.value, depth)}`;
-      case 'different':
-        return [`${preSpace}- ${node.name}: ${stringify(node.valueMinus, depth)}`,
-          `${preSpace}+ ${node.name}: ${stringify(node.valuePlus, depth)}`];
-      default:
-        throw new Error('Wrong type');
-    }
-  });
+    const result = node.map((obj) => {
+      const value = `${obj.name}: ${iterIfObject(obj.value)}`;
+      switch (obj.type) {
+        case 'plus':
+          return `${currentCloseIndent}+ ${value}`;
+        case 'minus':
+          return `${currentCloseIndent}- ${value}`;
+        case 'object':
+          return `${currentIndent}${obj.name}: ${iter(obj.children, depth + 1)}`;
+        case 'same':
+          return `${currentIndent}${value}`;
+        case 'different':
+          const valueBefore = `${currentCloseIndent}- ${obj.name}: ${iterIfObject(obj.valueMinus)}`;
+          const valueAfter = `${currentCloseIndent}+ ${obj.name}: ${iterIfObject(obj.valuePlus)}`;
+          return `${valueBefore}\n${valueAfter}`;
+      }
+      const keys = Object.keys(obj);
+      const nestedObj = keys.map((key) => `${currentIndent}${key}: ${iterIfObject(obj[key])}`);
 
-  return `\n${result.flatMap((str) => str).join('\n')}\n`;
+      return nestedObj.join('\n');
+    });
+
+    return [
+      '{',
+      ...result,
+      `${bracketIndent}}`,
+    ].join('\n');
+  };
+
+  return iter(tree, 1);
 };
 
 export default stylish;
